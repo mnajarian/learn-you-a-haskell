dacb9312f669a2fea33dc94778eea99bd4f6d4c5
Maya Najarian <maya.najarian@gmail.com>
2018-03-10 22:51:00 -0700

    Add Ch3 exercises

---
+++ 03-types-and-typeclasses.hs
@@ -0,0 +1,30 @@
+{-
+ - For this exercise, we are dealing with a type for colours of the rainbow
+ - The typeclass is defined here, and note its English spelling.
+ - For more information on how this is done, look ahead to:
+ - http://learnyouahaskell.com/making-our-own-types-and-typeclasses
+ -
+ - Have a play with the Colour in ghci, try the succ and pred functions and so on.
+ -}
+
+data Colour = Red | Orange | Yellow | Green | Blue | Indigo | Violet
+    deriving (Eq, Ord, Show, Bounded, Enum)
+
+{-
+ - The Colour typeclass is of type Ord
+ - What is the "first" (or least) colour
+ -}
+firstColour = minBound :: Colour
+
+-- List the colours in reverse order
+reverseColourOrder = reverse [minBound :: Colour .. maxBound :: Colour]
+
+{-
+ - Mix two colours together, to produce the average value of the two.
+ - Example: paintMix Orange Green = Yellow
+ - If necessary, favour the "higher" value when computing the average.
+ - For example: paintMix Green Violet = Indigo
+ - Hint: Integer division can be performed with the quot function: quot 7 2 = 3
+ -}
+paintMix c1 c2 = [c1 .. c2] !! quot (length [c1 .. c2]) 2
+-- idea here is that we can restrict the search to only the elements between c1 and c2 
